9e1a983a032e9ed67fb4dbef4166018cfcbbcc04
Nooaah <noah.chtl@gmail.com>
2020-10-15 15:14:38 +0200

    Create first visual graph

---
+++ graph.py
@@ -0,0 +1,51 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import datetime
+
+# Function to ask question
+
+
+def query_yes_no(question, default="yes"):
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+
+
+# Data for plotting
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2 * np.pi * t)
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+       title='Title of the graph')
+ax.grid()
+
+
+# Save in PNG ?
+if (query_yes_no("Save the graph in .png format ?", "no")):
+    fig.savefig("backups/test" + str(datetime.datetime.now()) + ".png")
+
+# Show graph
+plt.show()
